5ea216bf64f0621cdcb65c9eaa5671395788e7e8
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-15 18:31:14 -0400

    Create create_gpo_map_drive.ps1

---
+++ create_gpo_map_drive.ps1
@@ -0,0 +1,44 @@
+param (
+    [string]$GpoName,
+    [string]$GpoDescription,
+    [string]$SharePath,
+    [string]$DomainDn
+)
+
+Import-Module GroupPolicy
+
+# Create the GPO
+New-GPO -Name $GpoName -Comment $GpoDescription
+
+# Create the XML for the drive map
+$driveMapXML = @"
+<DriveMaps clsid="8AD5C1C7-F46C-4DFF-B9E5-A1C349BCEAAF" displayName="Snare-Products Drive" image="7" preference="true">
+    <Properties action="U" uid="2E143B8F-6B0D-4E8B-9239-BB1DC4AD8C34" driveLetter="P" path="$SharePath" hide="false" />
+</DriveMaps>
+"@
+
+# Create a temporary path to store the XML
+$gpoBackupPath = "C:\Temp\GPOBackups"
+if (-not (Test-Path $gpoBackupPath)) {
+    New-Item -ItemType Directory -Path $gpoBackupPath
+}
+
+# Backup the GPO
+Backup-GPO -Name $GpoName -Path $gpoBackupPath
+
+# Get the path to the GPO XML file
+$gpo = Get-GPO -Name $GpoName
+$gpoXMLPath = Join-Path -Path $gpoBackupPath -ChildPath $gpo.GPOID
+$gpoXMLPath = Join-Path -Path $gpoXMLPath -ChildPath "Machine\Preferences\Drives\Drives.xml"
+
+# Write the drive map XML to the XML file
+$driveMapXML | Out-File -FilePath $gpoXMLPath -Encoding ASCII
+
+# Restore the GPO from the modified backup
+Restore-GPO -Name $GpoName -Path $gpoBackupPath -Replace
+
+# Clean up temporary backup path
+Remove-Item -Recurse -Force -Path $gpoBackupPath
+
+# Link the GPO to the domain
+New-GPLink -Name $GpoName -Target $DomainDn
